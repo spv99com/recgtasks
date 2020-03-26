9bc6db67eb98f72831c122d26eb73a6ff314af65
Jozef Sovcik <jozef@sovcik.net>
2020-03-26 07:20:32 +0100

    folder cleanup

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/config.sh
-source $DIR/version
+source $DIR/.settings
+source $DIR/.version
 cd src
 clasp push
 clasp deploy --deploymentId $publishDeploymentId --description "$VERSION@$BUILD"
