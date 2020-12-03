f7d780c8eaf4fbabdcd13b0744d98e1329bf60f6
Jozef Sovcik <jozef@sovcik.net>
2020-12-03 09:44:39 +0100

    fixing git commit

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "BUILD=$BUILD" > $DIR/.version
 echo "VERSION=$VERSION" >> $DIR/.version
 sed -i -E "s/(recgversion.*[(]v)(.*)([)].*)/\1$VERSION@$BUILD\3/g" src/index.html
 sed -E "s/^var codeBuild.*;/var codeBuild = '$BUILD';/s;t;d" src/Code.js
-git commit -m "version bump $VERSION@$BUILD"
+git commit . -m "version bump $VERSION@$BUILD"
 cd src
 clasp push
