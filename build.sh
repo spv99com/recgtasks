5cd9de5b7ceb99dfe14528d9daea9791613bc90b
Jozef Sovcik <jozef@sovcik.net>
2020-12-03 09:46:49 +0100

    version bump 2.0.1@145

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ BUILD=$((BUILD+1))
 echo "BUILD=$BUILD" > $DIR/.version
 echo "VERSION=$VERSION" >> $DIR/.version
 sed -i -E "s/(recgversion.*[(]v)(.*)([)].*)/\1$VERSION@$BUILD\3/g" src/index.html
-sed -E "s/^var codeBuild.*;/var codeBuild = '$BUILD';/s;t;d" src/Code.js
+sed -E "s/^var codeBuild.*;/var codeBuild = '$BUILD';/" src/Code.js
 git commit . -m "version bump $VERSION@$BUILD"
 cd src
 clasp push
