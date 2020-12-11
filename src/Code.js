1e7a59999077b9ec20c36f42b18e94d09853763d
Jozef Sovcik <jozef@sovcik.net>
2020-12-11 07:54:43 +0100

    version bump 2.0.1@159

--- src/Code.js
+++ src/Code.js
@@ -22,7 +22,7 @@ var userToday = new Date();
 
 var userEmail = "xxxxx";
 
-var codeBuild = '158';  // code build number automatically updated by build script
+var codeBuild = '159';  // code build number automatically updated by build script
 
 //*****************************************
 //*****************************************
