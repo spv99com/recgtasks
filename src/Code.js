e2a3278359746debb59021d250d3fdecb21a710e
Jozef Sovcik <jozef@sovcik.net>
2020-12-27 08:11:30 +0100

    version bump 2.1.0@175

--- src/Code.js
+++ src/Code.js
@@ -9,7 +9,7 @@ var userToday = new Date();
 
 var userEmail = "xxxxx";
 
-var codeBuild = '174';  // code build number automatically updated by build script
+var codeBuild = '175';  // code build number automatically updated by build script
 
 /**
  * Special function that handles HTTP GET requests to the published web app.
