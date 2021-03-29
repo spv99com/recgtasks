9c43af27c08b28f7f167a084d31c4b72dd713417
Jozef Sovcik <jozef@sovcik.net>
2021-03-29 16:37:01 +0200

    fixing clasp

--- publish.sh
+++ publish.sh
@@ -8,9 +8,20 @@ then
   exit 1
 fi
 
+if [ ! -f ~/.clasprc.json -a ! -f ./clasprc.json ]; then
+  echo "Saving CLASP credentials to local file"
+  echo $CLASP_ID > ./clasprc.json
+  SAVED_CLASP=1
+fi
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/.settings
 source $DIR/.version
 
 yarn clasp push -f
 yarn clasp deploy --deploymentId $publishDeploymentId --description "$VERSION@$BUILD"
+
+if [ $SAVED_CLASP -eq 1 ]; then
+  rm ./clasprc.json
+fi
