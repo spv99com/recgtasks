8b72d9036a31ec70daa867d91a291dcd88267d76
sovcik <jsdev2015@gmail.com>
2021-03-29 15:19:06 +0000

    github-actions: version bump

--- src/Code.js
+++ src/Code.js
@@ -9,7 +9,7 @@ var userToday = new Date();
 
 var userEmail = "xxxxx";
 
-var codeBuild = '189';  // code build number automatically updated by build script
+var codeBuild = '190';  // code build number automatically updated by build script
 
 /**
  * Special function that handles HTTP GET requests to the published web app.
